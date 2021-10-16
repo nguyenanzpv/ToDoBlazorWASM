8f26a72e51841d97b5525c7cc506364faed4aeb2
nguyenanzpv <nguyenanzpv@gmail.com>
2021-10-16 14:07:42 +0700

    comit lesson 09: create api

---
+++ TodoListApi/Data/TodoListDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using TodoListApi.Entities;
+
+namespace TodoListApi.Data
+{
+    public class TodoListDbContext:DbContext
+    {
+        public TodoListDbContext(DbContextOptions<TodoListDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Task> Tasks { get; set; }
+    }
+}
---
+++ TodoListApi/Enums/Status.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoListApi.Enums
+{
+    public enum Status
+    {
+        New,
+        InProgress,
+        Resolved,
+        Done
+    }
+}
